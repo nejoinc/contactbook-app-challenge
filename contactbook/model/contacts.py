4c3ef3ea31ca902df4d5fe5492aeb534bf1b0334
nejoinc <josemanuelvalenciavelez@gmail.com>
2025-09-15 16:36:05 -0500

    Add ContactBook class

--- contactbook/model/contacts.py
+++ contactbook/model/contacts.py
@@ -1,1 +1,12 @@
-# TODO: Add code here
+from dataclasses import dataclass, field
+
+
+@dataclass
+class ContactBook:
+    contacts: dict[str, Contact] = field(default_factory=dict)
+
+    def add_contact(self, name: str, phone: str, email: str, tags: list[str]):
+        new_object = Contact(name, phone, email, tags)
+        self.contacts[new_object.phone] = new_object
+
+    def delete_contact(self, phone: str):
